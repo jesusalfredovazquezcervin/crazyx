c7ccd283c15ebaaafbf6d8c8a933ffa83517aaa1
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-02-03 10:43:59 -0600

    se agrega el metodoupdateTotalScore para calcular el total de puntos de un jugador

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,11 @@
 class Player < ApplicationRecord
     CATEGORY= %w[1st 2nd 3rd 4th 5th] 
     has_many :events
+
+    def updateTotalScore 
+        #Compute all the gainned points and update the totalScore field
+        total_score = Score.where(player_id: self.id).collect{|score| score.points }.sum
+        self.totalScore = total_score
+        self.save!
+    end
 end
