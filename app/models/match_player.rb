b72aa3773b07a04108c50b144bdb2ea4f02aff31
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-02-08 21:58:24 -0600

    se agrega el metodo setStatus

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -3,4 +3,17 @@ class MatchPlayer < ApplicationRecord
   belongs_to :event
   belongs_to :player
   STATUS= %w[OnBoard OnHold Canceled] #Status
+
+  def setStatus 
+    #Set the status to the MatchPlayer depending on the qty players who have been signed in into the event
+    event = Event.find(self.event_id)
+    #We get the onboard players
+    obPlayers = MatchPlayer.where(event_id: event.id).select{|mp| mp.status== 'onBoard'}.count
+    if obPlayers < event.people
+      self.status = 'OnBoard'
+    elsif obPlayers >= event.people
+      self.status = 'OnHold'
+    end    
+  end
 end
+
