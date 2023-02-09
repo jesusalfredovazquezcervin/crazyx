44fe5649dc0b930a0a659ada7be42b1e8d537033
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-02-09 11:24:48 -0600

    se agregan los campos de tiempo al event_params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,6 +65,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:name, :eventDate, :people, :status, :winner)
+      params.require(:event).permit(:name, :eventDate, :people, :status, :winner, :timeIni, :timeEnd)
     end
 end
