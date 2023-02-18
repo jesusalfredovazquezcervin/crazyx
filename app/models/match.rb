0f514bf1603cd964f062e040151de93efd77d67c
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-02-18 16:45:22 -0600

    se agrega un metodo que devuelve quien ganó un match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,21 @@
 class Match < ApplicationRecord
   belongs_to :event
+
+
+  def match_result
+    #returns if the match is: {even(0) Team1(1) Team2(2)} Team 
+    #Team 1.- PlayerOne|PlayerTwo Team 2.- PlayerThree|PlayerFour
+    result = 0
+    if !self.pointsOne.nil? && !self.pointsThree.nil?
+      if self.pointsOne > self.pointsThree
+        result = 1
+      elsif self.pointsOne < self.pointsThree
+        result = 2
+      end
+    else
+      result = -1 # There is no points
+    end
+    
+    return result
+  end
 end
