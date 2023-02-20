26fd49556991223c1c3a603526ac6434c8638d02
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-02-19 21:11:43 -0600

    se m odifica la ruta para el index y se agrega la destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :scores
   resources :match_players, except: [:new, :index]
-  resources :matches, except: [:index]
+  resources :matches, except: [:index, :destroy]
   resources :events
   resources :players
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   get 'match_players/new/:id', to: "match_players#new", as: 'new_match_player'
   # route to show the players for a specific event
   get 'match_players/index/:id', to: "match_players#index", as: 'players_enrroled'
-  get 'matches/index/:id', to: "matches#index", as: 'event_matches'
+  get 'matches/index/:id/', to: "matches#index", as: 'event_matches'
+  delete "matches/:event_id/:round", to: "matches#destroy", as: "matches_destroy_round"
+  get 'matches/create_round_of_matches/:event_id', to: "matches#create_round_of_matches", as: 'create_round_of_matches'
 end
