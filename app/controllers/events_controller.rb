230d7cb32f12e293d4390e2b5cdfac73793edd50
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-02-27 14:58:21 -0600

    Se agregan los campos extras de category (level)y mixed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,6 +65,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:name, :eventDate, :people, :status, :winner, :timeIni, :timeEnd)
+      params.require(:event).permit(:name, :eventDate, :people, :status, :winner, :timeIni, :timeEnd, :mixed, :level)
     end
 end
