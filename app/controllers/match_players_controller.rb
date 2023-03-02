a7a1dca447b59dec1294cdfd796db3daf0f3a8a1
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-03-02 12:08:11 -0600

    Se crean los nuevos metodos para validar codigos

--- app/controllers/match_players_controller.rb
+++ app/controllers/match_players_controller.rb
@@ -1,5 +1,5 @@
 class MatchPlayersController < ApplicationController
-  before_action :set_match_player, only: %i[ show edit update destroy ]  
+  before_action :set_match_player, only: %i[ show edit destroy edit update ] 
   # GET /match_players or /match_players.json
   def index
     @match_players = MatchPlayer.where(event_id: params[:id])
@@ -13,33 +13,19 @@ class MatchPlayersController < ApplicationController
 
   # GET /match_players/new
   def new
-    #logger.debug "hello ID----> #{params}"
-    
-    
     @event = Event.find(params[:id])    
     @match_player = MatchPlayer.new
     render layout: "empty"
   end
 
-  # GET /match_players/1/edit
-  def edit
-  end
-
   # POST /match_players or /match_players.json
   def create
-    #logger.debug "hello here we have the event id----> #{match_player_params[:event_id]}"
-    #@match_player = MatchPlayer.new(match_player_params)
-    #@match_player.setStatus
-    #logger.debug "----------------- The status of the player is ----> #{@match_player.status}"
-
     #Validations
     mp = MatchPlayer.create(match_player_params)
     respond_to do |format|      
       if !mp.errors.messages.any?      
         mp.setStatus
-        #logger.debug "----------------- The status of the player is ----> #{mp.status}"
         mp.save!
-        #logger.debug "-----------The id of the player saved is ---------------> #{mp.id}"
         format.html { redirect_to match_player_url(mp), notice: "You have joined succesfully to the event"}
         format.json { render :show, status: :created, location: @match_player }
       else
@@ -52,18 +38,55 @@ class MatchPlayersController < ApplicationController
       end
     end
   end
+  
+  # GET /match_players/1/edit
+    def edit
+    #Debo validar la existencia de algun codigo previo. 
+    #Eliminar todos los existentes y crear uno nuevo y despues enviar este codigo a la pantalla de editar    
+    @match_player.player.VerificationCodes.where(used: false).destroy_all
+    @verification_code = VerificationCode.create(code: SecureRandom.random_number(1000..9999), player_id: @match_player.player_id, event_id: @match_player.event_id)
+    
+    #We must send the verification code to the cellphone
+    message = "This is your verification code: " << @verification_code.code.to_s
+    sms = Message.new(number: @match_player.player.cellphone, body: message, action: "edit_verification_code", controller: "MatchPlayersController")
+    result = sms.send_sms
+    sms.error = result.error_message
+    sms.save!
 
-  # PATCH/PUT /match_players/1 or /match_players/1.json
+    flash.now[:notice] = "We've send you another code, please check your messages!'" if params[:resend] == "true"
+    #logger.debug "------------- Hello there resend = True #{params[:resend]}    ------------"
+    render(layout: "empty")
+    
+  end
+  
+    # PATCH/PUT /match_players/1 or /verification_codes/1.json
   def update
-    respond_to do |format|
-      if @match_player.update(match_player_params)
-        format.html { redirect_to match_player_url(@match_player), notice: "Match player was successfully updated." }
-        format.json { render :show, status: :ok, location: @match_player }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @match_player.errors, status: :unprocessable_entity }
-      end
-    end
+      @verification_code = VerificationCode.find(params[:match_player][:verification_id])
+      if params[:match_player][:code].to_i == @verification_code.code    
+        #se actualiza el codigo a usado=true
+        @verification_code.used = true
+        
+        #se elimina al player del evento
+        @match_player.destroy
+        respond_to do |format|    
+          if @verification_code.save
+            #format.html { redirect_to verification_code_url(@verification_code), notice: "Verification code was successfully updated." }
+            format.html { redirect_to new_match_player_path(@match_player.event_id), notice: "The player has been successfully withdrawn from the event." }
+            #format.json { render :show, status: :ok, location: @verification_code }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @verification_code.errors, status: :unprocessable_entity }
+            format.turbo_stream { render :form_update, status: :unprocessable_entity }        
+          end
+        end  
+      else      
+        respond_to do |format|
+          flash.now[:notice] = "The verification code does not match or is no longer valid. Try again."
+          format.html { render :edit, status: :unprocessable_entity, notice: "The verification code does not match or is no longer valid. Try again." }
+          format.json { render json: @verification_code.errors, status: :unprocessable_entity }
+          format.turbo_stream { render :form_update, status: :unprocessable_entity }  
+        end        
+      end      
   end
 
   # DELETE /match_players/1 or /match_players/1.json
@@ -83,6 +106,6 @@ class MatchPlayersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def match_player_params
-      params.require(:match_player).permit(:event_id, :player_id, :status)
+      params.require(:match_player).permit(:event_id, :player_id, :status, :code)
     end
 end
