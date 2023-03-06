a36285b63e70b0dabf8b427e31f64bb026a094a8
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-03-06 15:32:08 -0600

    Se generan las tablas necesarias para controlar las imagenes.

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js" #original
 pin "@popperjs/core", to: "http://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
