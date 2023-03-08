f877a38bc649e098aaf30c579f9c1c500409cbb3
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-03-08 16:41:48 -0600

    Se agrega la ruta para el edit de matches para poder enviar y recibir el round que se está actualizando.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :verification_codes
   resources :scores
   resources :match_players, except: [:new, :index, :edit]
-  resources :matches, except: [:index, :destroy]
+  resources :matches, except: [:index, :destroy, :edit]
   resources :events
   resources :players
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -14,10 +14,11 @@ Rails.application.routes.draw do
   get 'match_players/new/:id', to: "match_players#new", as: 'new_match_player'
   # route to show the players for a specific event
   get 'match_players/index/:id', to: "match_players#index", as: 'players_enrolled'
-  get 'matches/index/:id/', to: "matches#index", as: 'event_matches'
+  get 'matches/index/:id/(:round)', to: "matches#index", as: 'event_matches'
   delete "matches/:event_id/:round", to: "matches#destroy", as: "matches_destroy_round"
   get 'matches/create_round_of_matches/:event_id', to: "matches#create_round_of_matches", as: 'create_round_of_matches'
   get "match_players/edit/:id/(:resend)", to: "match_players#edit", as: "edit_match_player"
   patch "events/update_status/:id", to: "events#update_status", as: "update_status_event"
   get "event/show_closed_event/:id", to: "events#show_closed_event", as: "show_closed_event"
+  get "matches/edit/:id/:round", to: "matches#edit", as: "edit_match"
 end
