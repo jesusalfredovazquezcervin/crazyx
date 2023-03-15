ad3b95fd9af685bacce4c2f699146ccb903eba28
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-03-15 15:57:53 -0600

    se agrupoan las rutas de match_player y se reordenan las deplayers para que puedan funcionar bien el new cuando se registra enrola un nuevo player o cuando se crea un player normal.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   resources :matches, except: [:index, :destroy, :edit]
   resources :events
   
-  
-  get "players/new/(:event_id)", to: "players#new", as: "new_player"
-  resources :players, except: [:new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -15,14 +12,28 @@ Rails.application.routes.draw do
 
   # route for a new match-player with a specific event
   get 'match_players/new/:id', to: "match_players#new", as: 'new_match_player'
+  get "match_players/edit/:id/(:resend)", to: "match_players#edit", as: "edit_match_player"
   # route to show the players for a specific event
   get 'match_players/index/:id', to: "match_players#index", as: 'players_enrolled'
+
+  
+  
   get 'matches/index/:id/(:round)', to: "matches#index", as: 'event_matches'
   delete "matches/:event_id/:round", to: "matches#destroy", as: "matches_destroy_round"
   get 'matches/create_round_of_matches/:event_id', to: "matches#create_round_of_matches", as: 'create_round_of_matches'
-  get "match_players/edit/:id/(:resend)", to: "match_players#edit", as: "edit_match_player"
+  
   patch "events/update_status/:id", to: "events#update_status", as: "update_status_event"
   get "event/show_closed_event/:id", to: "events#show_closed_event", as: "show_closed_event"
   get "matches/edit/:id/:round", to: "matches#edit", as: "edit_match"
   get "scores/index/:id", to: "scores#index", as: "player_scores"
-end
+  
+  get "players/new/(:event_id)", to: "players#new", as: "new_player"
+  resources :players, except: [:new]
+  
+  #resources :players do
+  #  get "players/new/(:event_id)", to: "players#new", as: "new_player", on: :collection
+  #end
+  
+  
+  
+end 
