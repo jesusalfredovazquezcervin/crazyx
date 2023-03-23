c8c491b53c3b2bd5ba28318b3722d494003d0711
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-03-23 15:51:47 -0600

    se comenta para que ya envie mensajes a los telefonos de los jugadores reales.

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ApplicationRecord
     def send_sms
         #This method will send an sms message
         #number = "52" << self.number # Uncomment this line to work in production        
-        number = "524461327380" #comment this line to work in production        
+        #number = "524461327380" #comment this line to work in production        
         account_sid = ENV['TWILIO_ACCOUNT_SID']
         auth_token = ENV['TWILIO_AUTH_TOKEN']
         messaging_service_sid= ENV['TWILIO_MSG_SERVICE_ID']
