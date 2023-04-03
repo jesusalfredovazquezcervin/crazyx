2237e8e6a0511bfacf906907debbaaa668be7570
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-04-03 13:31:37 -0600

    Se modifica la zona horaria para que funcione correctamente el countdown.

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Crazyx
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Mexico City"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
