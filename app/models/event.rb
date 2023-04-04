5fdc756852e71cec0f0672acc69ed4d4057d9341
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-04-04 10:40:12 -0600

    se agrega el helper para obtener las url

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+    include Rails.application.routes.url_helpers
     STATUS= %w[Open Closed] #Status
     belongs_to :player, optional: true
     attribute :status, :string, default: 'Open'
@@ -8,6 +9,7 @@ class Event < ApplicationRecord
     has_many :verification_codes
     has_many :results
     has_many :couples
+    attribute :message_sent, :boolean, default: false
     def getPlayersPoints
         # Obtain the sumatory of points for all the players for the event {player, total_points} {1=>9, 2=>20, 3=>15, 4=>14, 5=>1, 6=>1}
         id = self.id
