fbb3efd90913e0b43c8cf2692691a7cdc5d71318
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-04-12 16:22:05 -0600

    se agregan los controllers, rutas y modelo para user devise

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+  def create
+    user = User.where(:email => params[:user][:email])
+    if (user.size > 0)
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+
+      role = user[0].role
+      case role # was case obj.class
+        when "Admin"
+          respond_with resource, location: after_sign_in_path_for(resource)
+        when "Player"
+          respond_with resource, location: dashboard_player_path(resource.player_id)        
+      end      
+    else
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    end
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << /[a-z0-9-.]+\.ngrok\.io/
   Rails.application.routes.default_url_options[:host] = "localhost:3000"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
