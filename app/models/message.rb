b11b7f2f2f47eeca7a276760b4f0b7c9af827e83
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-08-13 10:30:40 -0600

    se agrega el nuevo numero para prueba

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,11 +5,13 @@ class Message < ApplicationRecord
         #This method will send an sms message
         number = "52" << self.number # Uncomment this line to work in production        
         #number = "524461327380" #comment this line to work in production        
-        account_sid = ENV['TWILIO_ACCOUNT_SID']
-        auth_token = ENV['TWILIO_AUTH_TOKEN']
+        #number = "524422859104" #comment this line to work in production        
+        account_sid =  ENV['TWILIO_ACCOUNT_SID']
+        auth_token =  ENV['TWILIO_AUTH_TOKEN']
         messaging_service_sid= ENV['TWILIO_MSG_SERVICE_ID']
         @client = Twilio::REST::Client.new(account_sid, auth_token) 
         sms = @client.messages.create(body: self.body,  messaging_service_sid: messaging_service_sid, to: number) 
+        logger.debug("Message has been send: '#{self.body} ' ")
         return sms
 
     end
