160a71c60f62072454d4425a9f6290543654cbed
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2023-08-15 12:05:32 -0600

    se agrega el controller.js para la validacion de eventos

---
+++ app/javascript/controllers/event_validation_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+import { get } from "@rails/request.js"
+
+
+// Connects to data-controller="event-validation"
+export default class extends Controller {
+  connect() {
+    console.debug("It works!")
+  }
+  change(event) {    
+    let date = document.getElementById("event_eventDate").value;
+    let start = document.getElementById("event_timeIni").value ;
+    let category = document.getElementById("select2_category").value;
+    /*console.log("------------------- DATE ------------------")
+    console.log(date);
+    console.log("------------------- Start Time ------------------")
+    console.log(start);
+    console.log("------------------- CATEGORY ------------------")
+    console.log(category);*/
+    if ((date != "") && (start != "") &&(category != "")) {            
+      get(`/events/event_validation?date=${date}&start=${start}&category=${category}`, {
+        responseKind: "turbo-stream"
+      })    
+    }else{
+      console.log("Some variable is missing")
+    }
+    
+  }
+}
