0375050408b053b9ee016f60276402ec1d090011
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2024-04-09 16:13:34 -0600

    se modifican porque se generó un nuevo ambiente de produccion

--- Capfile
+++ Capfile
@@ -37,4 +37,4 @@ require "capistrano/passenger"
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
 set :rbenv_type, :user
-set :rbenv_ruby, "3.2.2"
+set :rbenv_ruby, "3.1.2"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "146.190.55.149", user: "deploy", roles: %w{app db web}
+server "64.23.195.71", user: "deploy", roles: %w{app db web}
 
 # role-based syntax
 # ==================
