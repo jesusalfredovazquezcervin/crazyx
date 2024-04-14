d49f5d117f28e7d15a095e7c820d64487d17c007
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2024-04-13 20:15:33 -0600

    se modifica el logger debug a info para ver el mensaje que se ha enviado.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
     
     def send_sms
         #This method will send an sms message
-        number = "52" << self.number # Uncomment this line to work in production        
+        number = "52" << self.number # Uncomment this line to work in production
         #number = "524461327380" #comment this line to work in production        
         #number = "524422859104" #comment this line to work in production        
         account_sid =  ENV['TWILIO_ACCOUNT_SID']
@@ -11,7 +11,7 @@ class Message < ApplicationRecord
         messaging_service_sid= ENV['TWILIO_MSG_SERVICE_ID']
         @client = Twilio::REST::Client.new(account_sid, auth_token) 
         sms = @client.messages.create(body: self.body,  messaging_service_sid: messaging_service_sid, to: number) 
-        logger.debug("Message has been send: '#{self.body} ' ")
+        logger.info("Message has been send: '#{self.body} ' ")
         return sms
 
     end
